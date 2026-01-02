41affcbda6849ec3135620ce7c3ddbe9da96fd48
HETUL SANJAYKUMAR KATARIA <iamhetul911@gmail.com>
2026-01-02 16:03:41 +0530

    fix: added missing BaseModel and Presidio imports

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from fastapi import FastAPI, HTTPException, Security, Depends
 from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
 
 app = FastAPI()
 
